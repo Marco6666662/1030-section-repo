cb4b98ff116ff5c9cc304122ef8c58d3d38b2410
DorneichI <129298065+DorneichI@users.noreply.github.com>
2023-04-18 16:33:51 -0400

    Added John 5 user story

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -275,3 +275,20 @@ def get_promotion(proj_num, proj_name, prod_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+# Get all products purchased in an order from the database
+# John - 5
+@customers.route('/order_products/<order_id>', methods=['GET'])
+def get_order_prods(order_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from order_products op JOIN products p on op.product_id = p.prod_id where op.order_id = {0}'.format(order_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
