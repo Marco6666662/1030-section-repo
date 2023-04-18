314f5392c07ee299fbda51fdb1bb55863be01cee
usaydsheikh <sheikh.u@northeastern.edu>
2023-04-18 17:30:57 -0400

    retailer routes 1-3 tested and working

--- flask-app/src/retailers/retailers.py
+++ flask-app/src/retailers/retailers.py
@@ -9,7 +9,7 @@ retailers = Blueprint('retailers', __name__)
 @retailers.route('/retailers_products/', methods=['GET'])
 def get_retailers_products():
     cursor = db.get_db().cursor()
-    cursor.execute('select * from products join offered_by on products.prod_id=offered_by.product_id')
+    cursor.execute('select * from products join retailers_products on products.prod_id=retailers_products.prod_id')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -22,49 +22,43 @@ def get_retailers_products():
 
 # Add new product
 @retailers.route('/products/<prod_id>', methods=['POST'])
-def post_prod_id(date_listed, name, _class, category, price, description, brought_id, order_id, prod_id):
+def post_prod_id(prod_id):
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+
+    date_listed = req_data['date_listed']
+    name = req_data['name']
+    _class = req_data['class']
+    category = req_data['category']
+    price = req_data['price']
+    description = req_data['description']
+
     cursor = db.get_db().cursor()
-    cursor.execute(f'insert into products (date_listed, name, class, category, price, description, brought_id, order_id, prod_id) \
-                   values ({date_listed}, {name}, {_class}, {category}, {price}, {description}, {brought_id}, {order_id}, {prod_id});')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    cursor.execute(f'insert into products (date_listed, name, class, category, price, description, prod_id) \
+                   values ({date_listed}, {name}, {_class}, {category}, {price}, {description}, {prod_id});')
+    db.get_db().commit()
+    return 'Success'
 
 # Update the price of a product
 @retailers.route('/products/<prod_id>', methods=['PUT'])
-def update_prod_id(prod_id, price):
+def update_prod_id(prod_id):
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+
+    price = req_data['price']
+
     cursor = db.get_db().cursor()
     cursor.execute(f'update products set price = {price} where prod_id = {prod_id};')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    db.get_db().commit()
+    return 'Success'
 
 # Delete a product that is discontinued
 @retailers.route('/products/<prod_id>', methods=['DELETE'])
 def delete_prod_id(prod_id):
     cursor = db.get_db().cursor()
     cursor.execute(f'delete from products where prod_id = {prod_id};')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    
+    return 'Success'
 
 # Return all detailed information for a particular product
 @retailers.route('/products/<prod_id>', methods=['GET'])
